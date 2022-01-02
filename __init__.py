9ba6e6ea34831e671bfd18d7edfcdb2d89707fd9
Geonuk Lee <kunwooklee27@gmail.com>
2022-01-02 13:23:12 +0900

    Expose main_train.

--- __init__.py
+++ __init__.py
@@ -2,5 +2,6 @@ from main_3D_BoNet import *
 from helper_data_s3dis import *
 from helper_data_plot import *
 from main_eval import *
+from main_train import *
 from helper_net import *
 from main_eval import *
